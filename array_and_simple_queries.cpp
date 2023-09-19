c3a811a650856a2c55c0e9ebd5fe203149302f42
Lê Thanh Minh <90665329+SagitaKDX@users.noreply.github.com>
2023-09-19 07:48:20 +0700

    Add files via upload

---
+++ array_and_simple_queries.cpp
@@ -0,0 +1,126 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+typedef long long  ll;
+typedef long double ld;
+
+typedef pair<int, int> pi;
+typedef pair<ll, int> pli;
+typedef pair<int, ll> pil;
+typedef pair<ll,ll> pl;
+typedef pair<ld,ld> pd;
+
+typedef vector<int> vi;
+typedef vector<bool> vb;
+typedef vector<ld> vd;
+typedef vector<ll> vl;
+typedef vector<pi> vpi;
+typedef vector<pl> vpl;
+typedef vector<pli> vli;
+typedef vector<pil> vil;
+
+#define FORE(i, v) for (__typeof((v).begin()) i = (v).begin(); i != (v).end(); i++)
+#define BIT(x, i) (((x) >> (i)) & 1)
+#define MASK(i) (1LL << (i))
+#define mp make_pair
+#define pb push_back
+#define f first
+
+// #define s second
+#define lb lower_bound
+#define ub upper_bound
+#define sz(x) (int)x.size()
+#define ALL(x) begin(x), end(x)
+
+void minimize(long long &x,long long y){
+   if(x>y){
+      x=y;
+   }
+}
+
+void maximize(long long &x,long long y){
+    if(x<y){
+        x=y;
+    }
+}
+
+template <typename T> inline void read(T & x)
+{
+    char c; bool nega=0;
+    while((!isdigit(c=getchar()))&&c!='-');
+    if(c=='-')
+    {
+        c=getchar();
+        nega=1;
+    }
+    x=c-48;
+    while(isdigit(c=getchar()))
+    {
+        x=x*10+c-48;
+    }
+    if(nega) x=-x;
+}
+template <typename T> void Write(T x) {if (x > 9) Write(x/10); putchar(x%10+48);}
+template <typename T> void write(T x) {if (x < 0) {putchar('-'); x = -x;} Write(x);}
+
+long long pow_mod(long long a, long long b, long long m) {
+     long long res = 1;
+     while(b){
+            res = res * (b & 1 ? a : 1) % m;
+            a = a * a % m; b >>= 1;
+     }
+     return res;
+}
+
+long long GCD(long long a , long long b){
+    while(b != 0 ){
+         a = a % b;
+         long long tmp = a;
+         a = b;
+         b = tmp;
+    }
+    return a;
+}
+
+long long minn(long long a ,long long b , long long c ,long long d){
+    long long res = a;
+    if(res > d) res = d;
+    if(res > b) res = b;
+    if(res > c) res = c;
+    return res;
+}
+const ll INF = 1e20 + 7;
+const ll MAXN = 1e5 + 7;
+const ll MOD = 1e9 + 7;
+
+void reorder(vector<int> &inp,int l,int r, int command) {
+    vector<int> tmp(inp.begin()+l,inp.begin()+r);
+    inp.erase(inp.begin()+l,inp.begin()+r);
+    if(command==1){
+        inp.insert(inp.begin(),tmp.begin(),tmp.end());
+    }else if(command==2) {
+        inp.insert(inp.end(),tmp.begin(),tmp.end());
+    }
+}
+
+int main() {
+    
+    int N,M;
+    // get N , M
+    std::cin>>N>>M;
+  vector<int> input(N);
+  for(int i=0; i<N; ++i) {
+      cin >> input[i];
+  }
+   
+   while(M--){
+       int r,l,c;
+       cin>>c>>l>>r;
+       reorder(input, l-1, r, c);
+   }
+    cout<<abs(input[input.size()-1]-input[0])<<endl;
+    for(const auto &i: input) cout<<i<<" ";
+    
+   
+    return 0;
+}
