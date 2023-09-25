5f8947fd639ff31d67813e9a063d19a597ae2a4b
Lê Thanh Minh <90665329+SagitaKDX@users.noreply.github.com>
2023-09-25 09:18:03 +0700

    Add files via upload

---
+++ querieswithfixedlength.cpp
@@ -0,0 +1,129 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+typedef long long  ll;
+typedef long double ld;
+
+typedef pair<int, int> pi;
+typedef pair<ll, int> pli;
+typedef pair<int, ll> pil;
+typedef pair<ll,ll> pl;
+typedef pair<ld,ld> pd;
+
+typedef vector<int> vi;
+typedef vector<bool> vb;
+typedef vector<ld> vd;
+typedef vector<ll> vl;
+typedef vector<pi> vpi;
+typedef vector<pl> vpl;
+typedef vector<pli> vli;
+typedef vector<pil> vil;
+
+#define FORE(i, v) for (__typeof((v).begin()) i = (v).begin(); i != (v).end(); i++)
+#define bit(x, i) (((x) >> (i)) & 1)
+#define MASK(i) (1LL << (i))
+#define mp make_pair
+#define pb push_back
+#define f first
+
+// #define s second
+#define lb lower_bound
+#define ub upper_bound
+#define sz(x) (int)x.size()
+#define ALL(x) begin(x), end(x)
+
+void minimize(long long &x,long long y){
+   if(x>y){
+      x=y;
+   }
+}
+
+void maximize(long long &x,long long y){
+    if(x<y){
+        x=y;
+    }
+}
+
+template <typename T> inline void read(T & x)
+{
+    char c; bool nega=0;
+    while((!isdigit(c=getchar()))&&c!='-');
+    if(c=='-')
+    {
+        c=getchar();
+        nega=1;
+    }
+    x=c-48;
+    while(isdigit(c=getchar()))
+    {
+        x=x*10+c-48;
+    }
+    if(nega) x=-x;
+}
+template <typename T> void Write(T x) {if (x > 9) Write(x/10); putchar(x%10+48);}
+template <typename T> void write(T x) {if (x < 0) {putchar('-'); x = -x;} Write(x);}
+
+long long pow_mod(long long a, long long b, long long m) {
+     long long res = 1;
+     while(b){
+            res = res * (b & 1 ? a : 1) % m;
+            a = a * a % m; b >>= 1;
+     }
+     return res;
+}
+
+long long GCD(long long a , long long b){
+    while(b != 0 ){
+         a = a % b;
+         long long tmp = a;
+         a = b;
+         b = tmp;
+    }
+    return a;
+}
+
+long long minn(long long a ,long long b , long long c ,long long d){
+    long long res = a;
+    if(res > d) res = d;
+    if(res > b) res = b;
+    if(res > c) res = c;
+    return res;
+}
+const ll INF = 1e18 + 7;
+const ll MAXN = 1e5 + 7;
+const ll MOD = 1e9;
+int n ,q;
+ll a[MAXN];
+int main(){
+    // ios_base::sync_with_stdio(false);
+    // cin.tie(0);
+    freopen("new.inp" , "r" , stdin);
+    freopen("new.out" , "w" , stdout);
+    read(n); read(q);  
+    for(int i = 1 ; i <= n ; i ++){
+        read(a[i]);
+    } 
+    deque<ll > dq;
+    while(q--){
+        int d;
+        read(d);
+        ll min_val = INF;
+        for(int i = 1 ; i <= n ; i ++){
+            while(dq.size() && a[dq.back()] < a[i]){
+                dq.pop_back();
+            }
+            dq.push_back(i);
+            if(dq.front() <= i - d){
+                dq.pop_front();
+            }
+            if(i >= d){
+                minimize(min_val , a[dq.front()]);
+            }
+        }
+        write(min_val); putchar('\n');
+        while(dq.size()){
+            dq.pop_back();
+        }   
+    }
+    return 0;
+}
