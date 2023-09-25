b3781220c1e79870b7fd68a72653a827d1cade88
Lê Thanh Minh <90665329+SagitaKDX@users.noreply.github.com>
2023-09-25 11:23:31 +0700

    Add files via upload

---
+++ subsequenceweighting.cpp
@@ -0,0 +1,203 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+typedef long long  ll;
+typedef long double ld;
+
+typedef pair<int, int> pi;
+typedef pair<ll, int> pli;
+typedef pair<int, ll> pil;
+typedef pair<ll,ll> pl;
+typedef pair<ld,ld> pd;
+
+typedef vector<int> vi;
+typedef vector<bool> vb;
+typedef vector<ld> vd;
+typedef vector<ll> vl;
+typedef vector<pi> vpi;
+typedef vector<pl> vpl;
+typedef vector<pli> vli;
+typedef vector<pil> vil;
+
+#define FORE(i, v) for (__typeof((v).begin()) i = (v).begin(); i != (v).end(); i++)
+#define bit(x, i) (((x) >> (i)) & 1)
+#define MASK(i) (1LL << (i))
+#define mp make_pair
+#define pb push_back
+#define f first
+
+// #define s second
+#define lb lower_bound
+#define ub upper_bound
+#define sz(x) (int)x.size()
+#define ALL(x) begin(x), end(x)
+
+void minimize(long long &x,long long y){
+   if(x>y){
+      x=y;
+   }
+}
+
+void maximize(long long &x,long long y){
+    if(x<y){
+        x=y;
+    }
+}
+
+template <typename T> inline void read(T & x)
+{
+    char c; bool nega=0;
+    while((!isdigit(c=getchar()))&&c!='-');
+    if(c=='-')
+    {
+        c=getchar();
+        nega=1;
+    }
+    x=c-48;
+    while(isdigit(c=getchar()))
+    {
+        x=x*10+c-48;
+    }
+    if(nega) x=-x;
+}
+template <typename T> void Write(T x) {if (x > 9) Write(x/10); putchar(x%10+48);}
+template <typename T> void write(T x) {if (x < 0) {putchar('-'); x = -x;} Write(x);}
+
+long long pow_mod(long long a, long long b, long long m) {
+     long long res = 1;
+     while(b){
+            res = res * (b & 1 ? a : 1) % m;
+            a = a * a % m; b >>= 1;
+     }
+     return res;
+}
+
+long long GCD(long long a , long long b){
+    while(b != 0 ){
+         a = a % b;
+         long long tmp = a;
+         a = b;
+         b = tmp;
+    }
+    return a;
+}
+
+long long minn(long long a ,long long b , long long c ,long long d){
+    long long res = a;
+    if(res > d) res = d;
+    if(res > b) res = b;
+    if(res > c) res = c;
+    return res;
+}
+const ll INF = 1e18 + 7;
+const ll MAXN = 1e5 + 7;
+const ll MOD = 1e9;
+int n , d;
+ll a[MAXN] , w[MAXN];
+ll b[MAXN] , pos[MAXN];
+ll st[MAXN * 4];
+void rebuild(int id , int l , int r ){
+    st[id] = -INF;
+    if(l == r){
+        return ;
+    }
+    int mid = l + r;
+    mid >>= 1;
+    rebuild(id << 1 , l , mid);
+    rebuild(id << 1 | 1  , mid + 1 , r); 
+}
+void update(int id , int l , int r , int pos , ll val){
+    if(l == r){
+        st[id] = val;
+        return;
+    }
+    int mid = l + r;
+    mid >>= 1 ;
+    if(pos <= mid){
+        update(id << 1 , l , mid , pos , val);
+    }else{
+        update(id << 1 | 1 , mid + 1 , r , pos , val);
+    }
+    st[id] = max(st[id << 1] , st[id << 1 | 1]);
+}
+ll get(int id , int l , int r , int u , int v){
+    if(u >  r || l > v){
+        return -INF;
+    }
+    if(u <= l && r <= v){
+        return st[id];
+    }
+    int mid = l + r;
+    mid >>= 1;
+    return max(get(id << 1 , l , mid , u , v) , get(id << 1 | 1 , mid + 1 , r , u , v));
+
+}
+int binarysearch(ll val){
+    int l = 1 , r = n;
+    int mid = l + r;
+    mid >>= 1;
+    while(l <= r){
+        mid = l + r;
+        mid >>= 1;
+        if(b[mid] == val){
+            return mid;
+        }
+        if( val > b[mid]){
+            l = mid + 1;
+        }else{
+            r = mid - 1;
+        }
+    }
+    return 0;
+}
+int main(){
+    ios_base::sync_with_stdio(false);
+    cin.tie(0);
+    freopen("new.inp" , "r" , stdin);
+    freopen("new.out" , "w" , stdout);
+    int t;
+    cin >> t;
+
+    while(t--){
+        cin >> n ;
+        for(int i = 1 ; i <= n ; i ++){
+            cin >> a[i];
+            b[i] = a[i];
+        }
+        for(int i = 1 ; i <= n ; i ++){
+            cin >> w[i];
+        }
+        sort(b + 1 ,  b + n + 1);
+        d = 1 ;
+        for(int i = 1 ; i <= n ;i ++){
+            if(i >= 2 && b[i] != b[i - 1]){
+                d ++;
+            }
+            pos[i] = d;
+        }
+        rebuild(1 , 1 , d);
+        ll z;
+        ll cur = 0;
+        ll res = -INF;
+        for(int i = 1 ; i <= n ; i ++){
+            z = binarysearch(a[i]);
+            z = pos[z];
+            cur = 0;
+            if(z > 1){
+                cur = get(1 , 1 , d , 1 , z -1);
+                cur = ((cur == -INF) ? 0 : cur);
+            }
+            cur += w[i];
+            maximize(res , cur);
+            ll val = get(1 , 1 , d , z , z);
+            if(cur < val){
+                continue;
+            }else{
+    
+                update(1 , 1 , d , z , cur);
+            }
+        }
+        cout << res << '\n';
+    }
+    return 0;
+}
