bb85d8bb7cf7e970ae7de9f46ffd3b3c5dae4d9e
Lê Thanh Minh <90665329+SagitaKDX@users.noreply.github.com>
2024-02-19 14:47:10 +0700

    Create 25D.cpp

---
+++ 25D.cpp
@@ -0,0 +1,177 @@
+#include <bits/stdc++.h>
+ 
+using namespace std;
+typedef long long  ll;
+typedef long double ld;
+ 
+typedef pair<int, int> pi;
+typedef pair<ll, int> pli;
+typedef pair<int, ll> pil;
+typedef pair<ll,ll> pl;
+typedef pair<ld,ld> pd;
+ 
+typedef vector<int> vi;
+typedef vector<bool> vb;
+typedef vector<ld> vd;
+typedef vector<ll> vl;
+typedef vector<pi> vpi;
+typedef vector<pl> vpl;
+typedef vector<pli> vli;
+typedef vector<pil> vil;
+ 
+#define BIT(x, i) (((x) >> (i)) & 1)
+#define MASK(i) (1LL << (i))
+#define mp make_pair
+#define pb push_back
+#define f first
+ 
+#define s second
+#define lb lower_bound
+#define ub upper_bound
+#define sz(x) (int)x.size()
+#define ALL(x) begin(x), end(x)
+ 
+void minimize(long long &x,long long y){
+   if(x>y){
+      x=y;
+   }
+}
+ 
+void maximize(long long &x,long long y){
+    if(x<y){
+        x=y;
+    }
+}
+ 
+template <typename T> inline void read(T & x)
+{
+    char c; bool nega=0;
+    while((!isdigit(c=getchar()))&&c!='-');
+    if(c=='-')
+    {
+        c=getchar();
+        nega=1;
+    }
+    x=c-48;
+    while(isdigit(c=getchar()))
+    {
+        x=x*10+c-48;
+    }
+    if(nega) x=-x;
+}
+template <typename T> void Write(T x) {if (x > 9) Write(x/10); putchar(x%10+48);}
+template <typename T> void write(T x) {if (x < 0) {putchar('-'); x = -x;} Write(x);}
+ 
+long long pow_mod(long long a, long long b, long long m) {
+     long long res = 1;
+     while(b){
+            res = res * (b & 1 ? a : 1) % m;
+            a = a * a % m; b >>= 1;
+     }
+     return res;
+}
+ 
+long long GCD(long long a , long long b){
+    while(b != 0 ){
+         a = a % b;
+         long long tmp = a;
+         a = b;
+         b = tmp;
+    }
+    return a;
+}
+ 
+long long minn(long long a ,long long b , long long c ,long long d){
+    long long res = a;
+    if(res > d) res = d;
+    if(res > b) res = b;
+    if(res > c) res = c;
+    return res;
+}
+const ll MAXN = 1e3 + 7 ;
+const ll MOD = 1e9 + 7;
+const ll INF = 1e18 + 7;
+int n;
+vector<int> adj[MAXN];
+int u , v;
+struct item{
+    int u , v , id;
+};
+item canh[MAXN];
+bool vis[MAXN];
+int par[MAXN];
+int erase[MAXN];
+int findpar(int u){
+    return par[u] < 0 ? u : par[u] = findpar(par[u]);
+}
+
+
+bool join(int u , int v){
+    u = findpar(u);
+    v = findpar(v);
+    if(u == v){
+        return false;
+    }
+    if(par[u] > par[v]){
+        swap(u , v);
+    }
+    par[u] += par[v];
+    par[v] = u;
+    return true;
+}
+
+void DFS(int u){
+    vis[u] = true;
+    for(int v : adj[u]){
+        if(vis[v]) continue;
+        DFS(v);
+    }
+}
+
+void solve(){
+    cin >> n;
+    for(int i = 1 ; i < n ; i ++){
+        cin >> u >> v;
+        canh[i].u = u;
+        canh[i].v = v;
+        canh[i].id = i;
+        adj[u].push_back(v);
+        adj[v].push_back(u);
+        par[i] = -1;
+    }
+    par[n] = -1;    
+    int num = 0;
+    for(int i = 1 ; i < n ; i ++){
+        if(!join(canh[i].u , canh[i].v)){
+            num ++;
+            erase[num] = canh[i].id;
+        }
+    }
+    cout << num << '\n';
+    if(num == 0){
+        return ;
+    }
+    int last= -1 , num2 = 0;
+    for(int i = 1 ; i <= n ; i ++){
+        if(!vis[i]){
+            DFS(i);
+            if(last != -1){
+                num2 ++;
+               cout << canh[erase[num2]].u << " "<< canh[erase[num2]].v << ' ' << last << " " << i << '\n'; 
+            }
+            last = i;
+        }
+    }
+}
+signed main(){
+    ios_base::sync_with_stdio(false);
+    cin.tie(0);
+    // freopen("new.inp" , "r" , stdin);
+    // freopen("new.out" , "w" , stdout);
+    int t;
+    t = 1;    
+    while(t--){
+        solve();
+    }
+    return 0;
+}
