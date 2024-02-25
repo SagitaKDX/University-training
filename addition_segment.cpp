8257aa3d714ad06dba9e43adeda7645bfdd08274
Lê Thanh Minh <90665329+SagitaKDX@users.noreply.github.com>
2024-02-25 13:23:00 +0700

    Create addition_segment.cpp

---
+++ addition_segment.cpp
@@ -0,0 +1,162 @@
+#include <bits/stdc++.h>
+ 
+using namespace std;
+typedef long long  ll;
+typedef long double ld;
+ 
+typedef pair<int, int> pi;
+typedef pair<ll, int> pli;
+typedef pair<int, ll> pil;
+typedef pair<ll,ll> pl;
+typedef pair<ld,ld> pd;
+ 
+typedef vector<int> vi;
+typedef vector<bool> vb;
+typedef vector<ld> vd;
+typedef vector<ll> vl;
+typedef vector<pi> vpi;
+typedef vector<pl> vpl;
+typedef vector<pli> vli;
+typedef vector<pil> vil;
+ 
+#define BIT(x, i) (((x) >> (i)) & 1)
+#define MASK(i) (1LL << (i))
+#define mp make_pair
+#define pb push_back
+#define f first
+ 
+#define s second
+#define lb lower_bound
+#define ub upper_bound
+#define sz(x) (int)x.size()
+#define ALL(x) begin(x), end(x)
+ 
+void minimize(long long &x,long long y){
+   if(x>y){
+      x=y;
+   }
+}
+ 
+void maximize(long long &x,long long y){
+    if(x<y){
+        x=y;
+    }
+}
+ 
+template <typename T> inline void read(T & x)
+{
+    char c; bool nega=0;
+    while((!isdigit(c=getchar()))&&c!='-');
+    if(c=='-')
+    {
+        c=getchar();
+        nega=1;
+    }
+    x=c-48;
+    while(isdigit(c=getchar()))
+    {
+        x=x*10+c-48;
+    }
+    if(nega) x=-x;
+}
+template <typename T> void Write(T x) {if (x > 9) Write(x/10); putchar(x%10+48);}
+template <typename T> void write(T x) {if (x < 0) {putchar('-'); x = -x;} Write(x);}
+ 
+long long pow_mod(long long a, long long b, long long m) {
+     long long res = 1;
+     while(b){
+            res = res * (b & 1 ? a : 1) % m;
+            a = a * a % m; b >>= 1;
+     }
+     return res;
+}
+ 
+long long GCD(long long a , long long b){
+    while(b != 0 ){
+         a = a % b;
+         long long tmp = a;
+         a = b;
+         b = tmp;
+    }
+    return a;
+}
+ 
+long long minn(long long a ,long long b , long long c ,long long d){
+    long long res = a;
+    if(res > d) res = d;
+    if(res > b) res = b;
+    if(res > c) res = c;
+    return res;
+}
+const ll MAXN = 2e5 + 7 ;
+const ll MOD = 1e8;
+const ll INF = 1e18 + 7;
+int n , m;
+int a[MAXN];
+struct item{
+    ll val , laz;
+};
+item st[MAXN * 4];
+void push_down(int id){
+    int t = st[id].laz;
+    st[id << 1].val += t;
+    st[id << 1].laz += t;
+    st[id << 1 | 1].val += t;
+    st[id << 1 | 1].laz += t;
+    st[id].laz = 0;
+}
+
+void update(int id , int l , int r , int u , int v , int val){
+    if(l > v || r < u){
+        return ;
+    }
+    if(u <= l && r <= v){
+        st[id].val += val;
+        st[id].laz += val;
+        return;
+    }
+    int mid = l + r;
+    mid >>= 1;
+    if(st[id].laz != 0){
+        push_down(id);
+    }
+    update(id << 1 , l , mid , u , v , val);
+    update(id << 1 | 1 , mid + 1 , r , u , v , val);
+}
+
+ll get_val(int id , int l , int r , int pos){
+    if(l == r){
+        return st[id].val;
+    }
+    int mid = l + r;
+    mid >>= 1;
+    if(st[id].laz != 0){
+        push_down(id);
+    }
+    if(pos <= mid){
+       return get_val(id << 1 , l , mid , pos);
+    }else{
+       return get_val(id << 1 | 1 , mid + 1 , r , pos);
+    }
+}
+signed main() {
+    ios::sync_with_stdio(false);
+    cin.tie(nullptr);
+    // freopen("new.inp" , "r" , stdin);
+    // freopen("new.out" , "w" , stdout);
+    cin >> n >> m;
+    int type , l , r , v;   
+    for(int i = 1 ; i <= m ; i ++){
+        cin >> type;
+        if(type - 1){
+            cin >> l;
+            l ++;
+            cout << get_val(1 , 1 , n , l) << '\n';
+        }else{
+            cin >> l >> r >> v;
+            l ++;
+            update(1 , 1 , n , l , r , v);
+        }
+    }
+    return 0;
+}
